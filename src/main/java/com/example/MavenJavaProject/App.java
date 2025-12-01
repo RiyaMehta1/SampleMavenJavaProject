9d5471ba8d1f5191b5dece80fc1f645006c2df99
user_name <user_email>
2025-12-01 18:22:35 +0530

    Trigger webhook test2

--- src/main/java/com/example/MavenJavaProject/App.java
+++ src/main/java/com/example/MavenJavaProject/App.java
@@ -5,6 +5,6 @@ package com.example.MavenJavaProject;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins webhook trigger1!");
+        System.out.println("Hello from Jenkins webhook trigger!");
     }
 }
